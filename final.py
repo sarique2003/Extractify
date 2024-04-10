81fe9ae2e2fca0456e1a400d6f8d86be44d65ca6
Sarique <93082071+sarique2003@users.noreply.github.com>
2024-04-10 12:50:23 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,69 +1,56 @@
-from grobid_client.grobid_client import GrobidClient
-from bs4 import BeautifulSoup
 import os
-from nltk.tokenize import sent_tokenize
+import spacy
+from bs4 import BeautifulSoup
 
-client = GrobidClient(config_path="./config.json")
-client.process("processFulltextDocument", "./resources/test", output="./",
-               consolidate_citations=True, tei_coordinates=True, force=True)
-ans = " "
-possibles = []
-freq = {}
+nlp = spacy.load("en_core_web_sm")  # Load spaCy's English language model
 
-
-def dataset(str):
-    words = str.split()
-    for word in words:
-        if word.isupper():
-            possibles.append(word)
+def dataset(text):
+    doc = nlp(text)
+    for ent in doc.ents:
+        if ent.label_ == "ORG":  # Check if entity is an organization
+            possibles.append(ent.text)
 
     for item in possibles:
-        if (item in freq):
+        if item in freq:
             freq[item] += 1
         else:
             freq[item] = 1
 
-
 def out(freq):
     datasets = max(freq, key=lambda x: freq[x])
-    print("Datasets used is:  ------------------------")
+    print("Datasets used is: ------------------------")
     print(datasets)
-
 
 items = ["dataset", "trained", "database", "experiments", "evaluated"]
 
-
-def check(str, count):
-    x = str.split(".")
+def check(text, count):
+    x = text.split(".")
     for i in range(len(x)):
         test = x[i]
         count = 0
         flag = False
         for j in range(len(items)):
-            if (items[j] in test):
+            if items[j] in test:
                 count += 1
-                if (count >= 1):
+                if count >= 1:
                     print(test)
                     dataset(test)
 
-
 search = True
-
 
 def extract(data):
     for info in data:
         ans = str(info.text)
         cnt = 0
-        if (search):
+        if search:
             flag = check(ans, cnt)
-
 
 path = './resources/test_out'
 for file in os.listdir(path):
-    possibles.clear()
-    freq.clear()
+    possibles = []  # Clear possibles list for each file
+    freq = {}  # Clear freq dictionary for each file
     if file.endswith(".tei.xml"):
-        with open(file, 'r') as f:
+        with open(os.path.join(path, file), 'r') as f:
             temp = f.read()
         soup = BeautifulSoup(temp, 'xml')
         data = soup.find_all('p')
